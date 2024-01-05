1d9c6a78cadd7e99e4b39e0714a466b55c9db5a9
unknown <ram23052002@gmail.com>
2024-01-05 11:27:15 +0530

    Adding print fun

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,13 +3,21 @@ using namespace std;
 
 int Sum(int x, int y)
 {
-	return x+y;
+   return x + y;
+}
+
+void print()
+{
+
+   cout << "working successfully";
 }
 
 int main()
 {
    // sum function
-   int sumValue = Sum(1,2);
+   int sumValue = Sum(1, 2);
    cout << sumValue << endl;
    cout << "Hello World";
+
+   print();
 }
