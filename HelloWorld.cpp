25e5a968335c484f150537f1a79adc520843d398
C0SMlC <pendurkarpratik04@gmail.com>
2024-01-05 12:13:52 +0530

    Adding Facorial Function

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,13 +3,21 @@ using namespace std;
 
 int Sum(int x, int y)
 {
-	return x+y;
+   return x + y;
+}
+
+int factorial(int num)
+{
+   if (num == 0 || num == 1)
+      return 1;
+   return num * factorial(num - 1);
 }
 
 int main()
 {
    // sum function
-   int sumValue = Sum(1,2);
+   int sumValue = Sum(1, 2);
    cout << sumValue << endl;
-   cout << "Hello World";
+   cout << "Hello World" << endl;
+   cout << factorial(5) << endl;
 }
