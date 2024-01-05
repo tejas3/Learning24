5e897b5067c729b8bba7fe5c00d22585594fd4e8
Tejas <jsarodt@gmail.com>
2024-01-05 10:49:51 +0530

    Add sum function

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 using namespace std;
 
+int Sum(int x, int y)
+{
+	return x+y;
+}
+
 int main()
 {
+   int sumValue = Sum(1,2);
+   cout << sumValue << endl;
    cout << "Hello World";
 }
