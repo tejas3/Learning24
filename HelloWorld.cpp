e244851aa9b285b5c23e11aabe58bbe47efcf167
Tanmay123-sp <tpatils20it@student.mes.ac.in>
2024-01-05 14:24:30 +0530

    new function added

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -5,6 +5,9 @@ int Sum(int x, int y)
 {
 	return x+y;
 }
+void show(){
+   cout<<"New function added";
+}
 
 int main()
 {
@@ -12,4 +15,5 @@ int main()
    int sumValue = Sum(1,2);
    cout << sumValue << endl;
    cout << "Hello World";
+   show();
 }
