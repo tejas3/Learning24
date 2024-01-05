1aa68597b3d6011ea89febad0a5bd9c3bc54349e
patiltejass <tp291101@gmail.com>
2024-01-05 12:49:33 +0530

    Remainder Function Added

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -5,11 +5,16 @@ int Sum(int x, int y)
 {
 	return x+y;
 }
+int remainder(int x, int y){
 
+   return x%y;
+}
 int main()
 {
    // sum function
    int sumValue = Sum(1,2);
    cout << sumValue << endl;
+   int remainderValue = remainder(1,2);
+   cout << remainderValue << endl;
    cout << "Hello World";
 }
